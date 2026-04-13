d648ef47378d1d48d3d9b1e2667d67b733429a15
Ulugbek Erkinov <ulugbek9015y@gmail.com>
2026-04-13 15:09:39 +0500

    Update pm2.config.js for Uvicorn and correct VPS path

--- pm2.config.js
+++ pm2.config.js
@@ -2,9 +2,10 @@ module.exports = {
   apps: [
     {
       name: "ai-interview-backend",
-      script: "../venv/bin/python",
-      args: "main.py",
-      cwd: "./",
+      script: "venv/bin/uvicorn",
+      args: "main:app --host 0.0.0.0 --port 8000",
+      cwd: "/var/www/workers/mentalaba/interview-insights-ai-back",
+      interpreter: "none",
       watch: false,
       env: {
         NODE_ENV: "production",
